d7034cf65645efe21f5fed8607ad0bfeeb5bc4e6
cloudguy003 <93338116+cloudguy003@users.noreply.github.com>
2022-11-06 22:47:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello world")
+var data = 0;
+  document.getElementById("counting").innerText = data;
+  function increment() {
+      data = data + 1;
+      document.getElementById("counting").innerText = data;
+  }
